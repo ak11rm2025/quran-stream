9b8128a28ab6b1e2dae08395aa5d8737979f956f
ak11rm2025 <akrm.zoomzx@gmail.com>
2025-11-24 21:54:47 -0800

    Update playlist.js

--- pages/api/playlist.js
+++ pages/api/playlist.js
@@ -3,33 +3,33 @@ export default function handler(req, res) {
     {
       id: 1,
       title: "تلاوة 1",
-      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran1.mp3",
+      file_url: "https://quran-stream-zeta.vercel.app/quran1.mp3",
       sort_order: 1
     },
     {
       id: 2,
       title: "تلاوة 2",
-      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran2.mp3",
+      file_url: "https://quran-stream-zeta.vercel.app/quran2.mp3",
       sort_order: 2
     },
     {
       id: 3,
       title: "تلاوة 3",
-      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran3.mp3",
+      file_url: "https://quran-stream-zeta.vercel.app/quran3.mp3",
       sort_order: 3
 
        },
        {
       id: 4,
       title: "تلاوة 4",
-      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran4.mp3",
+      file_url: "https://quran-stream-zeta.vercel.app/quran4.mp3",
       sort_order: 4
 
        },
        {
       id: 5,
       title: "تلاوة 5",
-      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran5.mp3",
+      file_url: "https://quran-stream-zeta.vercel.app/quran5.mp3",
       sort_order: 5
 
        },
