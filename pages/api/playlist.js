c712e01d3c2a2430b1c4ba83128f1d4b0193d22c
ak11rm2025 <akrm.zoomzx@gmail.com>
2025-11-24 21:35:44 -0800

    Create playlist.js

---
+++ pages/api/playlist.js
@@ -0,0 +1,43 @@
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 1,
+      title: "تلاوة 1",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran1.mp3",
+      sort_order: 1
+    },
+    {
+      id: 2,
+      title: "تلاوة 2",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran2.mp3",
+      sort_order: 2
+    },
+    {
+      id: 3,
+      title: "تلاوة 3",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran3.mp3",
+      sort_order: 3
+
+       },
+       {
+      id: 4,
+      title: "تلاوة 4",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran4.mp3",
+      sort_order: 4
+
+       },
+       {
+      id: 5,
+      title: "تلاوة 5",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran5.mp3",
+      sort_order: 5
+
+       },
+       {
+      id: 6,
+      title: "تلاوة 6",
+      file_url: "https://YOUR-VERCEL-DOMAIN.vercel.app/quran6.mp3",
+      sort_order: 6
+    }
+  ]);
+}
